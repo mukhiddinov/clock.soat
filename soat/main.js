5759a300b0a73f74444d4329fe51f03e1b81912e
mukhiddinov <muxiddinovxusniddin34@gmail.com>
2024-08-05 16:47:40 +0500

    js updated

--- soat/main.js
+++ soat/main.js
@@ -1,35 +1,28 @@
 let icon = document.getElementById('icon');
-let sekund = 0;
-let minut = 0;
-let soat = 0;
 
 const oclock = () => {
-  let interval = setInterval(function () {
-    sekund++;
-    let secondG = (360 / 60) * sekund; //secondG bu htmldagi sekundni gradusi
-    document.getElementById('sekund').style.transform = "rotate(" + secondG + "deg)";
-    if (sekund >= 60) {
-      minut++;
-      let minutG = (360 / 60) * minut;
-      document.getElementById('minut').style.transform = "rotate(" + minutG + "deg)";
-      sekund = 0;
-    }
-    if (minut >= 60) {
-      soat++;
-      let soatG = (360 / 12) * soat;
-      document.getElementById('soat').style.transform = "rotate(" + soatG + "deg)";
-      minut = 0;
-    }
-  },1000)
+    setInterval(function () {
+        let now = new Date();
+
+        let sekund = now.getSeconds();
+        let minut = now.getMinutes();
+        let soat = now.getHours();
+
+        let secondG = (360 / 60) * sekund;
+        let minutG = (360 / 60) * minut;
+        let soatG = (360 / 12) * soat + (30 / 60) * minut; // Adds minute offset to hour hand
+
+        document.getElementById('sekund').style.transform = "rotate(" + secondG + "deg)";
+        document.getElementById('minut').style.transform = "rotate(" + minutG + "deg)";
+        document.getElementById('soat').style.transform = "rotate(" + soatG + "deg)";
+    }, 1000);
 }
 
 icon.onclick = function() {
-	document.body.classList.toggle('dark-theme');
-	if (document.body.classList.contains('dark-theme')) {
-		icon.src = 'images/sun.png';
-	}
-	else {
-		icon.src = 'images/moon.png'
-	}
-
+    document.body.classList.toggle('dark-theme');
+    if (document.body.classList.contains('dark-theme')) {
+        icon.src = 'images/sun.png';
+    } else {
+        icon.src = 'images/moon.png';
+    }
 }
